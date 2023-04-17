d53d0bc743f59f64077542598af7d99a5973e1de
MaxKeable <max.keable488@gmail.com>
2023-04-17 14:27:11 +1000

    javascript written application working and added to read.me file

---
+++ Develop/README.md
@@ -0,0 +1,29 @@
+# This
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Description
+  hmmm
+  ## Table of contents
+  * [Descrition](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  
+* [License](#license)
+
+  ## Installation
+  ahhhh
+  ## Usage
+  does this work
+  ## Contributing
+  yeah
+  ## Tests
+  lets gooo
+  ## License
+
+        Licensed under the MIT license.
+  ## Questions
+  by maxkeable
+  * GitHub - [maxkeable](https://github.com/maxkeable)
+  * Please feel free to email the following email with any further questions:
+  * Email - keablemax@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+      return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+      return `\n* [License](#license)\n`;
+      }
+      return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+      return `## License
+
+        Licensed under the ${license} license.`;
+  }
+  return '';
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of contents
+  * [Descrition](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  ${renderLicenseLink(data.license)}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributions}
+  ## Tests
+  ${data.tests}
+  ${renderLicenseSection(data.license)}
+  ## Questions
+  by ${data.username}
+  * GitHub - [${data.username}](https://github.com/${data.username})
+  * Please feel free to email the following email with any further questions:
+  * Email - ${data.email}
 `;
 }
 
